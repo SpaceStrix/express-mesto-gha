da85cedbf29f9fd96939cedd16e8ec60c4252e7c
Deos <3xiksy@gmail.com>
2022-12-11 19:09:25 +0300

    ref: user routes

--- routes/cards.js
+++ routes/cards.js
@@ -5,11 +5,11 @@ const {
   getAllCards, createCard, deleteCard, likeCard, dislikeCard,
 } = require('../controllers/cards');
 
-router.get('/', getAllCards);
-router.post('/', express.json(), createCard);
-router.delete('/:cardId', deleteCard);
+router.get('/', getAllCards); //* Роут ко всем карточкам
+router.post('/', express.json(), createCard); //* Роут создания карточки
+router.delete('/:cardId', deleteCard); //* Роут удаления карточки
 
-router.put('/:cardId/likes', likeCard);
-router.delete('/:cardId/likes', express.json(), dislikeCard);
+router.put('/:cardId/likes', likeCard); //* Роут для утановки лайка
+router.delete('/:cardId/likes', express.json(), dislikeCard); //* Роут для удаления лайка
 
 module.exports = router;
