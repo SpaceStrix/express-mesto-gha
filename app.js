8a98b5f95983f11d7afc8722c6b8354b930a2d70
Deos <3xiksy@gmail.com>
2022-12-11 16:49:01 +0300

    fix: patch not found

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const { PORT = 3000 } = process.env;
 const app = express();
 
 mongoose.set('strictQuery', false); // убираем варнинг mongoose
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
 app.use((req, res, next) => {
   req.user = { _id: '6390f87b0405512d5565fcf6' };
@@ -14,16 +15,8 @@ app.use((req, res, next) => {
 app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
 
-mongoose.connect('mongodb://127.0.0.1:27017/mestodb')
-  .then(() => {
-    console.log('Successfully');
-  }).catch(() => {
-    console.log('Not Successfully');
-  });
-
-app.patch('*', (req, res) => {
-  res.send('what???', 404);
-});
+app.use('*', (req, res) => res.status(404)
+  .send({ message: '404 - Страница не найдена' }));
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
