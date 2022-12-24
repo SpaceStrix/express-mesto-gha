7b33d18aeb76853dbdb8e1556338e03e685dd0a0
KalabyYao <3xiksy@gmail.com>
2022-12-25 00:21:29 +0300

    fix: joi

--- app.js
+++ app.js
@@ -19,17 +19,14 @@ app.post('/signin', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
     password: Joi.string().required(),
-  }).unknown(true),
+  }),
 }), login);
 
 app.post('/signup', celebrate({
   body: Joi.object().keys({
-    name: Joi.string().required().min(2).max(30),
-    about: Joi.string().min(2).max(30),
-    avatar: Joi.string().required().min(18),
     email: Joi.string().required().email(),
     password: Joi.string().required().min(8),
-  }).unknown(true),
+  }),
 }), createUser);
 
 app.use(errors());
