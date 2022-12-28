6bba52cc02b59a41e5a979de2d7d5d0df0f69f64
KalabyYao <3xiksy@gmail.com>
2022-12-29 00:24:26 +0300

    fix: rev.1

--- app.js
+++ app.js
@@ -2,50 +2,24 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 
-const { celebrate, Joi, errors } = require('celebrate');
+const { errors } = require('celebrate');
 const router = require('./routes');
+const { handlerErr } = require('./middlewares/handlerErr');
+const { validationSignin, validationSignup } = require('./middlewares/validation');
 const { createUser, login } = require('./controllers/users');
 
-const { PORT = 3000 } = process.env;
+const { PORT, DB_CONNECT } = process.env;
 const app = express();
-
 mongoose.set('strictQuery', false); // убираем варнинг mongoose
-mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
+mongoose.connect(DB_CONNECT);
 
 app.use(express.json());
-app.post('/signin', celebrate({
-  body: Joi.object().keys({
-    email: Joi.string().required().email(),
-    password: Joi.string().required(),
-  }),
-}), login);
-app.post('/signup', celebrate({
-  body: Joi.object().keys({
-    name: Joi.string().min(2).max(30).default('Жак-Ив Кусто'),
-    about: Joi.string().min(2).max(30).default('Исследователь'),
-    avatar: Joi.string().default('https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png')
-      // eslint-disable-next-line no-useless-escape
-      .pattern(/^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$/)
-      .message('Некорректная ссылка'),
-    email: Joi.string().required().email(),
-    password: Joi.string().required().min(8),
-  }),
-}), createUser);
+app.post('/signin', validationSignin, login);
+app.post('/signup', validationSignup, createUser);
 app.use(router);
 
 app.use(errors());
-
-app.use((err, req, res, next) => {
-  const { statusCode = 500, message } = err;
-  res
-    .status(statusCode)
-    .send({
-      message: statusCode === 500
-        ? 'На сервере произошла ошибка'
-        : message,
-    });
-  next();
-});
+app.use(handlerErr);
 
 app.listen(PORT, () => {
   console.log(`Приложении запущено на ${PORT} порту`);
--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,9 @@ const NotFoundError = require('../errors/not-found-err');
 const usersRoutes = require('./users');
 const cardsRoutes = require('./cards');
 
-router.use('/users', auth, usersRoutes);
-router.use('/cards', auth, cardsRoutes);
+router.use(auth);
+router.use('/users', usersRoutes);
+router.use('/cards', cardsRoutes);
 
 router.use('*', (req, res, next) => { next(new NotFoundError('Указанный путь не найден')); });
 
